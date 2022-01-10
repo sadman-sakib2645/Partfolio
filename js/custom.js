9b3544734c91b75fbd35a8948b3bcb9e32badd55
sadman-sakib2645 <sakibsadman849@gmail.com>
2022-01-10 18:57:40 +0600

    just

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    var typed = new Typed('.type', {
+        // Waits 1000ms after typing "First"
+        strings: [
+            
+            'Mogan Michalvina.',
+            'A Graphics Designer.!',
+            'A Web Designer.!',
+            'A Web developer.!',
+            'A Freelancer.!',
+
+            ],
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 70,
+            showCursor: true,
+        });
+
+
+        $('.service-slider').slick({
+            slidesToShow:3,
+            autoplay:true,
+            prevArrow:'<i class="fas fa-long-arrow-alt-left left-icon"></i>',
+            nextArrow:'<i class="fas fa-long-arrow-alt-right right-icon"></i>',
+        });
+
+        var mixer = mixitup(".port-img");
+
+        new VenoBox({
+            selector: '.popup',
+            spinner:"wave",
+            spinColor: "black"
+        });
+    
+});

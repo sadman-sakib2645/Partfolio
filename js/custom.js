7450321d2464026ffd9f2bdb0afc20668624d7e6
sadman-sakib2645 <sakibsadman849@gmail.com>
2022-01-17 15:59:12 +0600

    na

--- js/custom.js
+++ js/custom.js
@@ -39,5 +39,38 @@ $(document).ready(function(){
             prevArrow:'<i class="fas fa-long-arrow-alt-left white icon1"></i>',
             nextArrow:'<i class="fas fa-long-arrow-alt-right white icon2"></i>',
         });
-    
+
+        $('.counter').counterUp({
+            delay: 5,
+            time: 1500
+        });
+
+        
+        AOS.init({
+            offset:500,
+        });
+
+       
+});
+$(window).scroll(function(){
+    var scrolling = $(this).scrollTop();
+
+    if(scrolling > 600){
+        $("#back-top").fadeIn();
+    }else{
+        $("#back-top").fadeOut();
+    }
+
+    if(scrolling > 500){
+        $("nav").addClass("nav-fix");
+    }else{
+        $("nav").removeClass("nav-fix");
+    }
+});
+
+// working setting
+$("#back-top").click(function(){
+    $("html,body").animate({
+        scrollTop: "0px" ,
+    },1500);
 });
